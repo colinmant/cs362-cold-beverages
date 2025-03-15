c6542b35bcd74f942c42a432bee851e81f6c9a49
Colin Mant <colinm6666@gmail.com>
2025-03-15 16:14:53 -0700

    Finished writing tests for water_reservoir_spec.rb added self. to the empty? and fill functions

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,27 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  it 'has a capacity and initial water volume' do
+    reservoir = WaterReservoir.new(10, 5)
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
 
+  it 'can be empty' do 
+    reservoir = WaterReservoir.new(10, 0)
+    expect(reservoir.empty?).to be true
+  end
+
+  it 'can be filled' do
+    reservoir = WaterReservoir.new(10, 0)
+    reservoir.fill
+    expect(reservoir.current_water_volume).to eq(10)
+  end
+
+  it 'can drain water' do
+    reservoir = WaterReservoir.new(10, 5)
+    reservoir.drain(3)
+    expect(reservoir.current_water_volume).to eq(2)
+  end
+  
 end
