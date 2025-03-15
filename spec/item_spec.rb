d04dd04e478b5c6be4bf01b2e2b0c24689e7ef7a
Colin Mant <colinm6666@gmail.com>
2025-03-15 12:43:08 -0700

    Added a test to verify that an item has a name and a volume upon initialization.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
-
+  it 'has a name and volume' do
+    item = Item.new('Banana', 3)
+    expect(item.name).to eq('Banana')
+    expect(item.volume).to eq(3)
+  end
 end
