950ad6b69ccc25fef8814e4b6e6d2d6954546ed2
Colin Mant <colinm6666@gmail.com>
2025-03-15 15:58:12 -0700

    Added tests to water_dispenser_spec. Added line vessel.fill to water_dispenser.rb dispense function to fill vessel while draining reservoir

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,23 @@
 require_relative '../lib/water_dispenser'
+require_relative '../lib/vessel'
+require_relative '../lib/water_reservoir'
+
 
 describe 'A water dispenser' do
 
+  it 'has a reservoir' do
+    reservoir = WaterReservoir.new(10, 5)
+    dispenser = WaterDispenser.new(reservoir)
+    expect(dispenser.reservoir).to eq(reservoir)
+  end
+
+  it 'fills a vessel' do
+    reservoir = WaterReservoir.new(10, 5)
+    vessel = Vessel.new('cup', 3)
+    dispenser = WaterDispenser.new(reservoir)
+    dispenser.dispense(vessel)
+    expect(vessel.empty?).to be false
+    expect(reservoir.current_water_volume).to eq(2)
+  end
+  
 end
